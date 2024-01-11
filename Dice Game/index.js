14997b1ba1f340f4e7f8362e53ce062575250d2c
Manish <122551932+ManishDev20@users.noreply.github.com>
2024-01-11 16:17:48 +0530

    index file uploaded

---
+++ Dice Game/index.js
@@ -0,0 +1,16 @@
+const random1 = Math.floor(Math.random() * 6 + 1);
+const random2 = Math.floor(Math.random() * 6 + 1);
+
+if (random1 > random2) {
+  document.querySelector("h1").innerHTML = "🚩player 1 wins!";
+} else if (random2 > random1) {
+  document.querySelector("h1").innerHTML = "player 2 wins!🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+
+const randomImageSource1 = "./images/dice" + random1 + ".png";
+const randomImageSource2 = "./images/dice" + random2 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
