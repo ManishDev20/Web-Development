21d38d6a88b8eeb3afd225df513feb85e33b6e74
Manish <122551932+ManishDev20@users.noreply.github.com>
2024-01-15 06:23:41 +0530

    Update index.js

--- Music Band/index.js
+++ Music Band/index.js
@@ -22,31 +22,31 @@ document.addEventListener("keydown", function (event) {
 function makeSound(key) {
   switch (key) {
     case "w":
-      var crash = new Audio("sounds/crash.mp3");
+      var crash = new Audio("./sounds/crash.mp3");
       crash.play();
       break;
     case "a":
-      var crash = new Audio("sounds/kick-bass.mp3");
+      var crash = new Audio("./sounds/kick-bass.mp3");
       crash.play();
       break;
     case "s":
-      var crash = new Audio("sounds/snare.mp3");
+      var crash = new Audio("./sounds/snare.mp3");
       crash.play();
       break;
     case "d":
-      var crash = new Audio("sounds/tom-1.mp3");
+      var crash = new Audio("./sounds/tom-1.mp3");
       crash.play();
       break;
     case "j":
-      var crash = new Audio("sounds/tom-2.mp3");
+      var crash = new Audio("./sounds/tom-2.mp3");
       crash.play();
       break;
     case "k":
-      var crash = new Audio("sounds/tom-3.mp3");
+      var crash = new Audio("./sounds/tom-3.mp3");
       crash.play();
       break;
     case "l":
-      var crash = new Audio("sounds/tom-4.mp3");
+      var crash = new Audio("./sounds/tom-4.mp3");
       crash.play();
       break;
     default:
